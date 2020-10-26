f9d5e567d1af2965e454efd37831d1efe0126033
areilly1 <58097168+areilly1@users.noreply.github.com>
2020-10-26 10:58:59 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,18 @@
+$(".mole1").click(function(){
+    $(".mole1").hide();
+});
+    
+$(".moleTwo").click(function(){
+    $(".moleTwo").hide();
+});
+    
+$(".thirdMole").click(function(){
+    $(".thirdMole").show();
+    $(".mole1").show();
+    $(".moleTwo").show();
+});
+    
+$(".reset").click(function(){
+    $("img").show();
+    
+});
